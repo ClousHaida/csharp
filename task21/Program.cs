e559eb6ac242819cdf74825263172f642bac5b6b
May <cloushaida@gmail.com>
2023-09-05 16:42:32 +1000

    task21

---
+++ task21/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите прошрамму, которая принимает на вход число () и выдает таблицу кубов чисел от 1 до N.
+3 -> 1, 4, 9
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Введите число N:");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+if (number < 0) number = number * -1;
+for (int i=1; i<number+1; i++)
+{
+    result =i*i*i;
+    Console.WriteLine(result);
+}

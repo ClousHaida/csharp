42131a085cecb9a4a1e229d93f407cf74d21daf7
May <cloushaida@gmail.com>
2023-09-11 19:49:23 +1000

    task21

--- task21/Program.cs
+++ task21/Program.cs
@@ -1,17 +1,27 @@
 ﻿/*
-Напишите прошрамму, которая принимает на вход число () и выдает таблицу квадратов чисел от 1 до N.
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+
 */
 
-Console.WriteLine("Введите число N:");
-int number = Convert.ToInt32(Console.ReadLine());
-int result = 0;
+int x1 = ReadInt("Введите x точки A: ");
+int y1 = ReadInt("Введите y точки A: ");
+int z1 = ReadInt("Введите z точки A: ");
+int x2 = ReadInt("Введите x точки B: ");
+int y2 = ReadInt("Введите y точки B: ");
+int z2 = ReadInt("Введите z точки B: ");
 
-if (number < 0) 
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z2 - z1;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine("Расстояние = " + length);
+
+int ReadInt(string message)
 {
-    number = number * (-1);
-}
-for (int i=1; i<=number; i++)
-{
-    result = i*i;
-    Console.WriteLine(result);
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }

97d186ce969bb21ec17b7d3c3e673b7c8157eba0
May <cloushaida@gmail.com>
2023-09-12 15:45:44 +1000

    task35

---
+++ task35/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+*/
+using System.ComponentModel.DataAnnotations;
+Console.Clear();
+
+int[] GetRandom(int arrayLength, int start, int end)
+{
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(start, end);
+    }
+    return array;
+}
+
+void PrintArray(int[] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+int QNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <= 99 && array[i] >= 10)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+// System.Console.WriteLine("Текст: ");
+int arrayL = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[] userArray = GetRandom(arrayL, min, max);
+int Q = QNumber(int[]array);
+
+//по какой-то причине не компилируется, пишет об ошибках, но я их не вижу.

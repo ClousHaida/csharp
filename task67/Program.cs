e8963ca29ce2ca00f0860a3c65ec0883f17547ea
May <cloushaida@gmail.com>
2023-09-19 17:39:24 +1000

    task67

---
+++ task67/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая будет принимать на вход число и возвращать сумму его цифрю.
+*/
+
+int result = SumNumber(246);
+int SumNumber(int n)
+{
+
+    if (n == 0)
+        return 0;
+    else return n % 10 + SumNumber(n / 10);
+
+}
+
+System.Console.WriteLine(result);

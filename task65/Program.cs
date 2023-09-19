9300cb639d109e0189470d8d087f62daba2d47d1
May <cloushaida@gmail.com>
2023-09-19 17:31:01 +1000

    task65

---
+++ task65/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задайте значение M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+*/
+
+void NaturalNum(int m, int n)
+{
+    if (m <= n)
+    {
+        System.Console.Write(m + ", ");
+        NaturalNum(m + 1, n);
+    }
+}
+NaturalNum(10, 17);

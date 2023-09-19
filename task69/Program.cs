eea5224cd03dda8967e3da18b6c6bdd1eb89f08e
May <cloushaida@gmail.com>
2023-09-19 17:43:27 +1000

    task69

---
+++ task69/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа А и В, и возводит А в целую степень В с помощью рекурсии.
+*/
+
+int result = ProdNumber(5, 2);
+
+int ProdNumber(int n, int m)
+{
+    if (m == 1)
+        return n;
+    else return n * ProdNumber(n, m - 1);
+}
+
+System.Console.WriteLine(result);
